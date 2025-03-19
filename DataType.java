0e0810a7d04ebff8fc09fc43b77a6034306ffcf7
BenjaminDaviesX <officialgeneralbeejay@gmail.com>
2025-03-19 13:56:58 +0100

    Primitive Data

---
+++ DataType.java
@@ -0,0 +1,36 @@
+public class DataType{
+	public static void main(String[] args){
+		//Integer type (byte, short, int, long) 
+		 //byte
+		byte age = 50;
+	System.out.printf("Your age is %d",age);
+	
+	    //Short
+	short quantity = 32000;
+    System.out.printf(" I bought %d quantity of bags for my business%n",quantity);
+	
+	//int
+	int populationInNigeria = 1000000000;
+	System.out.printf("The total population in Nigeria is %d%n",populationInNigeria);
+	
+	//long
+	long worldPopulation = 800000000L;
+	System.out.printf("The world population is %d%n",worldPopulation);
+	
+	//float-point type(float,double)
+	float myBalance = 123465.8585757F;
+	System.out.printf("Your account balance is %f%n",myBalance);
+	
+	//double
+	double cbnReserve = 5575857.646473738483526;
+	System.out.printf("The CBN balance is %.5f%n",cbnReserve);
+	
+	//Boolean type (boolean) 
+	boolean isJavaFun = true;
+    System.out.printf("Is Java a fun Language? %b",isJavaFun);
+	
+	// character Type (char)
+	char alpha = '@';
+	System.out.printf("My email is officialgeneralbeejay%cgmail.com",alpha);
+	}
+} 
