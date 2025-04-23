0dabcf4db52af034a7880af3f97da2b6501cc281
Benjamin Davies <officialgeneralbeejay@gmail.com>
2025-04-23 06:40:01 +0100

    Update ArrayBinarySearchMethod.java

--- Array Class Method/ArrayBinarySearchMethod.java
+++ Array Class Method/ArrayBinarySearchMethod.java
@@ -5,7 +5,7 @@ public class ArrayBinarySearchMethod {
         int[] numbers = {1, 2, 3, 5, 8}; // Sorted array
         int key = 3; // Number to find
 
-        int index = Arrays.binarySearch(numbers, key); // Search for the key
+        int index = Arrays.binarySearch(numbers, key);
 
         System.out.printf("Index: %d%n", index);
     }
